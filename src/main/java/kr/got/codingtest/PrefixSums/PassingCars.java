9d0315dbafc41e1cfba829dbf62193935f23ab1b
gotit <kgd891226@gmail.com>
2022-01-02 22:01:00 +0900

    추가

---
+++ src/main/java/kr/got/codingtest/PrefixSums/PassingCars.java
@@ -0,0 +1,21 @@
+package kr.got.codingtest.PrefixSums;
+
+/**
+ * 도로에서 지나가는 자동차의 수 세기.
+ * https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
+ *
+ * N개의 정수로 구성된 비어있지 않은 배열 A가 있다.
+ * 배열 A의 각 요소들은 0 또는 1로 연속되는 자동차를 나타냄.
+ *  - 0은 동쪽으로 여행하는 자동차
+ *  - 1은 서쪽으로 여행하는 자동차
+ *
+ */
+public class PassingCars {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution() {
+        return 0;
+    }
+}
